253bf187db8d4cdc7fae741c4b532712a643f396
nezinomas <nezinomas@gmail.com>
2026-03-13 17:20:15 +0200

    Update app.css and add taiwind.config

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    // This is where DaisyUI belongs!
+    plugins: [
+        forms,
+        require('daisyui'),
+    ],
+    
+    // You can also force your lofi theme here
+    daisyui: {
+        themes: ["lofi"],
+    },
+};
