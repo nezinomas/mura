d61c2f20506df8a99f6ee0725a87166ef6111d4b
Audrius <nezinomas@gmail.com>
2026-03-15 17:22:49 +0200

    Refactore QuoteController

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -5,14 +5,41 @@ namespace App\Http\Controllers;
 use App\Models\Quote;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class QuoteController extends Controller
+
+class QuoteController extends Controller implements HasMiddleware
 {
-    public function index()
+/**
+     * Get the middleware that should be assigned to the controller.
+     */
+    public static function middleware(): array
     {
-        return view('compose');
+        return [
+            new Middleware('throttle:5,1', only: ['store']),
+        ];
     }
 
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('quotes.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -29,6 +56,34 @@ class QuoteController extends Controller
         return redirect('/dashboard');
     }
 
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Quote $quote)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Quote $quote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
     public function destroy(Quote $quote)
     {
         Gate::authorize('delete', $quote);
