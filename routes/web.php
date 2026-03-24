fa4bcd9bcd8abdfa4e5898441638fd892d024004
nezinomas <nezinomas@gmail.com>
2026-03-24 10:55:27 +0200

    Delete Laravel welcome view

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
